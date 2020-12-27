f66e3c68dcac7bdc88001c7036a6404e0bde6a7b
J <james.hyojae.lee@gmail.com>
2020-12-27 13:05:23 -0800

    update

--- inhibition_analysis/inhibition_analysis.R
+++ inhibition_analysis/inhibition_analysis.R
@@ -86,17 +86,12 @@ nrow(distinct(below_avg_panc_AUC, ccl))
 panc_below_avg <- sort(table(below_avg_panc_AUC$Hugo_Symbol),decreasing=TRUE)[1:10]
 panc_below_avg
 
-
-
 #common mutation with above average AUC value
 above_avg_panc_AUC <- panc_in[panc_in$AUC >= 400, ]
 nrow(distinct(above_avg_panc_AUC, ccl))
 #list the 10 most common mutations
 panc_above_avg <- sort(table(above_avg_panc_AUC$Hugo_Symbol),decreasing=TRUE)[1:10]
 panc_above_avg
-
-
-
 
 # statistical significance of difference in AUC values between cells with that mutation and cells without the mutation
 
@@ -106,72 +101,130 @@ ttest_df$Hugo_Symbol[ttest_df$Hugo_Symbol != "TP53"] <- "Lacks Mut"
 ttest_df$Hugo_Symbol[ttest_df$Hugo_Symbol == "TP53"] <- "Has Mut"
 distinct(ttest_df, Hugo_Symbol)
 
+#save t-test results as a list
 t <- list()
 t[[1]] <- t.test(AUC ~ Hugo_Symbol, data = ttest_df)
 
-tt <- sapply(t, function(x) {
+#save p-value and means of the two groups
+tsum <- sapply(t, function(x) {
           c(x$estimate[1],
             x$estimate[2],
-            ci.lower = x$conf.int[1],
-            ci.upper = x$conf.int[2],
             p.value = x$p.value)
-        }
-      )
+        })
+tsum
 
 #create a function to repeat t-test for other genes
-
 ttest <- function(G){
   ttest_df <- panc_in
   ttest_df$Hugo_Symbol <- as.character(ttest_df$Hugo_Symbol)
   ttest_df$Hugo_Symbol[ttest_df$Hugo_Symbol != G] <- "Lacks Mut"
   ttest_df$Hugo_Symbol[ttest_df$Hugo_Symbol == G] <- "Has Mut"
   t.test(AUC ~ Hugo_Symbol, data = ttest_df)
+  
+  t <- list()
+  t[[1]] <- t.test(AUC ~ Hugo_Symbol, data = ttest_df)
+  tsum <- sapply(t, function(x) {
+                  c(x$estimate[1],
+                    x$estimate[2],
+                    p.value = x$p.value)
+                })
+  return(tsum)
 }
 
 ttest("KRAS")
-
 
 #create dataframe with top 10 mutations (with below 400 AUC)
 panc_below_avg_df <- as.data.frame(panc_below_avg, stringsAsFactors=FALSE)
 colnames(panc_below_avg_df) <- c('mutation', 'freq')
 
-panc_below_avg_df$mutated_mean <- NA
-panc_below_avg_df$others_mean <- NA
+panc_below_avg_df$Mut_AUC_mean <- NA
+panc_below_avg_df$others_AUC_mean <- NA
 panc_below_avg_df$p.value <- NA
 
-
-for (i in nrow(panc_below_avg_df)) {
+#for loop to fill in the dataframe 
+for (i in 1:nrow(panc_below_avg_df)) {
+  print(panc_below_avg_df$mutation[i])
   t <- ttest(panc_below_avg_df$mutation[i])
-  panc_below_avg_df$mutated_mean <- 
+  panc_below_avg_df$Mut_AUC_mean[i] <- t[1]
+  panc_below_avg_df$others_AUC_mean[i] <- t[2]
+  panc_below_avg_df$p.value[i] <- round(t[3], digits=3)
 }
 
-
-
-########################################################################################################################
-
-
+write.csv(panc_below_avg_df, "panc_mutation.csv", row.names=FALSE)
 
 
 
 ##################################################  Colorectal Cancer ################################################## 
 
-#common mutation with below average IC50
-below_avg_colo_IC50 <- colo_in[colo_in$Abs.IC50 < mean(colo_in$Abs.IC50), ]
-
-colo_top5_mut_IC50 <- sort(table(below_avg_colo_IC50$Hugo_Symbol),decreasing=TRUE)[1:10]
-colo_top5_mut_IC50
-# TP53 KRAS TTN KRT17 MT-ND5  
-
-#number of cell lines with below average IC50
-nrow(distinct(below_avg_colo_IC50, ccl, .keep_all = TRUE))
-
 #common mutation with below average AUC value
-below_avg_colo_AUC <- colo_in[colo_in$AUC < mean(colo_in$AUC), ]
-colo_top5_mut_AUC <- sort(table(below_avg_colo_AUC$Hugo_Symbol),decreasing=TRUE)[1:10]
-colo_top5_mut_AUC
-#same as the IC50 result
+below_avg_colo_AUC <- colo_in[colo_in$AUC < 400, ]
+nrow(distinct(below_avg_colo_AUC, ccl))
+#list the 10 most common mutations
+colo_below_avg <- sort(table(below_avg_colo_AUC$Hugo_Symbol),decreasing=TRUE)[1:10]
+colo_below_avg
 
-#number of  cell lines with below average AUC
-nrow(distinct(below_avg_colo_AUC, ccl, .keep_all = TRUE))
+#common mutation with above average AUC value
+above_avg_colo_AUC <- colo_in[colo_in$AUC >= 400, ]
+nrow(distinct(above_avg_colo_AUC, ccl))
+#list the 10 most common mutations
+colo_above_avg <- sort(table(above_avg_colo_AUC$Hugo_Symbol),decreasing=TRUE)[1:10]
+colo_above_avg
 
-########################################################################################################################
+# statistical significance of difference in AUC values between cells with that mutation and cells without the mutation
+
+ttest_df <- panc_in
+ttest_df$Hugo_Symbol <- as.character(ttest_df$Hugo_Symbol)
+ttest_df$Hugo_Symbol[ttest_df$Hugo_Symbol != "TP53"] <- "Lacks Mut"
+ttest_df$Hugo_Symbol[ttest_df$Hugo_Symbol == "TP53"] <- "Has Mut"
+distinct(ttest_df, Hugo_Symbol)
+
+#save t-test results as a list
+t <- list()
+t[[1]] <- t.test(AUC ~ Hugo_Symbol, data = ttest_df)
+
+#save p-value and means of the two groups
+tsum <- sapply(t, function(x) {
+  c(x$estimate[1],
+    x$estimate[2],
+    p.value = x$p.value)
+})
+tsum
+
+#create a function to repeat t-test for other genes
+ttest <- function(G){
+  ttest_df <- panc_in
+  ttest_df$Hugo_Symbol <- as.character(ttest_df$Hugo_Symbol)
+  ttest_df$Hugo_Symbol[ttest_df$Hugo_Symbol != G] <- "Lacks Mut"
+  ttest_df$Hugo_Symbol[ttest_df$Hugo_Symbol == G] <- "Has Mut"
+  t.test(AUC ~ Hugo_Symbol, data = ttest_df)
+  
+  t <- list()
+  t[[1]] <- t.test(AUC ~ Hugo_Symbol, data = ttest_df)
+  tsum <- sapply(t, function(x) {
+    c(x$estimate[1],
+      x$estimate[2],
+      p.value = x$p.value)
+  })
+  return(tsum)
+}
+
+ttest("KRAS")
+
+#create dataframe with top 10 mutations (with below 400 AUC)
+panc_below_avg_df <- as.data.frame(panc_below_avg, stringsAsFactors=FALSE)
+colnames(panc_below_avg_df) <- c('mutation', 'freq')
+
+panc_below_avg_df$Mut_AUC_mean <- NA
+panc_below_avg_df$others_AUC_mean <- NA
+panc_below_avg_df$p.value <- NA
+
+#for loop to fill in the dataframe 
+for (i in 1:nrow(panc_below_avg_df)) {
+  print(panc_below_avg_df$mutation[i])
+  t <- ttest(panc_below_avg_df$mutation[i])
+  panc_below_avg_df$Mut_AUC_mean[i] <- t[1]
+  panc_below_avg_df$others_AUC_mean[i] <- t[2]
+  panc_below_avg_df$p.value[i] <- round(t[3], digits=3)
+}
+
+write.csv(panc_below_avg_df, "colo_mutation.csv", row.names=FALSE)
